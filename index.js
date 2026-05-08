111cbc6ee9e010b11baa519f65bbd357e25e213b
Nathan <natanribeirobarbosa4161@gmail.com>
2026-05-08 05:44:47 -0300

    adicionando funcionalidade nova (deletar itens)

--- index.js
+++ index.js
@@ -1,91 +1,130 @@
 import { db } from "./firebase.js";
 
 import {
-  collection,
-  doc,
-  setDoc,
-  onSnapshot,
-   query,
-  where,
-  deleteDoc,
-  getDocs
+    collection,
+    doc,
+    setDoc,
+    onSnapshot,
+    query,
+    where,
+    deleteDoc,
+    getDocs
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-async function apagarPorNome(nome) {
+function captureCategories() {
 
     const ref = doc(db, "config", "allCategories");
     const snapshot = await getDoc(ref);
     const dados = snapshot.data();
 
-    const nomes = dados.names;
+    return dados.names;
+}
 
-    nomes.forEach(nome =>{
+async function apagarPorNome(nome) {
+
+    nomes = captureCategories;
+
+    nomes.forEach(nome => {
         const q = query(
-        collection(db, nome),
-        where("nome", "==", nome)
+            collection(db, nome),
+            where("nome", "==", nome)
         );
 
         const snapshot = await getDocs(q);
 
         snapshot.forEach(async (item) => {
-        await deleteDoc(doc(db, "roupas", item.id));
+            await deleteDoc(doc(db, "roupas", item.id));
         });
 
         console.log("Documentos removidos");
     })
-
-
-
-    
 }
 
 
 async function salvarProduto(nome, link, linkF, price, store, cat1, cat2, cat3, cat4) {
 
-  // cria um ID único manualmente
-  const produtoRef = doc(collection(db, "roupas"));
-  const produtoId = produtoRef.id;
+    // cria um ID único manualmente
+    const produtoRef = doc(collection(db, "roupas"));
+    const produtoId = produtoRef.id;
 
-  const dados = {
-    nome,
-    link,
-    linkF,
-    price,
-    store
-  };
+    const dados = {
+        nome,
+        link,
+        linkF,
+        price,
+        store
+    };
 
-  // salva na coleção principal
-  await setDoc(doc(db, "roupas", produtoId), dados);
+    // salva na coleção principal
+    await setDoc(doc(db, "roupas", produtoId), dados);
 
-  // salva na coleção da categoria com o MESMO ID
-  await setDoc(doc(db, cat1, produtoId), dados );
-  console.log("dados: "+cat2, cat3, cat4)
-    if(cat2!=''){
-        await setDoc(doc(db, cat2, produtoId), dados );
-  }
-    if(cat3!=''){
+    // salva na coleção da categoria com o MESMO ID
+    await setDoc(doc(db, cat1, produtoId), dados);
+    const categories = captureCategories;
+
+    if (categories.indexof(cat1) == -1) {
+        categories.push(cat1)
+
+        const ref = doc(db, "config", "allCategories");
+
+        await updateDoc(ref, {
+            names: nomesAtuais
+        });
+    }
+
+    if (cat2 != '') {
+        await setDoc(doc(db, cat2, produtoId), dados);
+            if (categories.indexof(cat2) == -1) {
+        categories.push(cat2)
+
+        const ref = doc(db, "config", "allCategories");
+
+        await updateDoc(ref, {
+            names: nomesAtuais
+        });
+    }
+    }
+    if (cat3 != '') {
         await setDoc(doc(db, cat3, produtoId), dados);
-  }
-    if(cat4!=''){
+            if (categories.indexof(cat3) == -1) {
+        categories.push(cat3)
+
+        const ref = doc(db, "config", "allCategories");
+
+        await updateDoc(ref, {
+            names: nomesAtuais
+        });
+    }
+    }
+    if (cat4 != '') {
         await setDoc(doc(db, cat4, produtoId), dados);
-  }
+            if (categories.indexof(cat4) == -1) {
+        categories.push(cat4)
 
-  await setDoc(doc(db, "vitrine", produtoId), dados);
+        const ref = doc(db, "config", "allCategories");
 
-  alert("Salvo com sucesso!");
+        await updateDoc(ref, {
+            names: nomesAtuais
+        });
+    }
+    }
+
+    await setDoc(doc(db, "vitrine", produtoId), dados);
+
+    alert("Salvo com sucesso!");
 }
 
 
 function carregarProdutos() {
-  const lista = document.getElementById("products")
+    const lista = document.getElementById("products")
 
-  onSnapshot(collection(db, "roupas"), snapshot => {
-    lista.innerHTML = ""
-    let html = '';
+    onSnapshot(collection(db, "roupas"), snapshot => {
+        lista.innerHTML = ""
+        let html = '';
 
-    snapshot.forEach(doc => {
-      const p = doc.data();
-      html += `
+        snapshot.forEach(doc => {
+            const p = doc.data();
+            html += `
        <div class="product">
 
       <div class="image" style="background-image: url('${p.linkF}')"></div>
@@ -103,10 +142,10 @@ function carregarProdutos() {
     
         `
 
+        })
+        lista.innerHTML = html;
+
     })
-    lista.innerHTML = html;
-    
-  })
 
 }
 
